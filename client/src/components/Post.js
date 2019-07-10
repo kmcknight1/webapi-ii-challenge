58c884727bcb378fb0ab21758ab89a1537d34e11
kmcknight1 <katiemcknight1991@gmail.com>
2019-07-10 11:04:54 -0500

    edit post modal added

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "@material-ui/core";
+import {
+  Card,
+  Modal,
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles,
+  Button,
+  TextField
+} from "@material-ui/core";
 import axios from "axios";
 
 import Comment from "./Comment";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: "absolute",
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(4),
+      outline: "none"
+    }
+  })
+);
+
 const Post = props => {
   const [comments, setComments] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [titleText, setTitleText] = useState("");
+  const [contentsText, setContentsText] = useState("");
+
+  const classes = useStyles();
 
   useEffect(() => {
     axios
@@ -19,8 +46,27 @@ const Post = props => {
       });
   }, []);
 
+  function editPost(body) {
+    axios
+      .put(`http://localhost:5000/api/posts/${props.post.id}`, body)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
-    <div className="post-card" style={{ margin: "1rem 4rem 1rem 4rem" }}>
+    <div className="post-card" style={{ margin: "1rem 5rem 1rem 5rem" }}>
       <Card>
         <h3>{props.post.title}</h3>
         <p
@@ -31,6 +77,9 @@ const Post = props => {
         >
           {props.post.contents}
         </p>
+        <Button variant="contained" onClick={handleOpen}>
+          Edit Post
+        </Button>
         {comments.length === 0 ? (
           <p style={{ fontSize: "0.9rem" }}>This post has no comments</p>
         ) : (
@@ -40,6 +89,57 @@ const Post = props => {
           <Comment comment={comment} index={index} />
         ))}
       </Card>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+          className={classes.paper}
+        >
+          <Typography variant="h5" id="modal-title">
+            Edit Post
+          </Typography>
+          <form
+            onSubmit={() => {
+              const body = {
+                title: titleText,
+                contents: contentsText
+              };
+              editPost(body);
+            }}
+          >
+            <TextField
+              variant="outlined"
+              label="Title"
+              value={titleText}
+              onChange={e => {
+                setTitleText(e.target.value);
+              }}
+            />
+            <TextField
+              variant="outlined"
+              label="Contents"
+              value={contentsText}
+              onChange={e => {
+                setContentsText(e.target.value);
+              }}
+            />
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Submit Changes</Button>
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 };
